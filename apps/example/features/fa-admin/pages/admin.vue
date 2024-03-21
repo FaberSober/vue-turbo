70c1b091fcb0b75cf0a436d308517022b11ef813
xupengfei <faberxu@gmail.com>
2024-03-21 14:08:40 +0800

    remove example temp

--- apps/example/features/fa-admin/pages/admin.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-  <h1>Admin</h1>
-
-  <RouterView />
-</template>
--- apps/example/features/fa-admin/pages/admin/index.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-  <main>
-    <h1>Feature 1 - Index</h1>
-  </main>
-</template>
