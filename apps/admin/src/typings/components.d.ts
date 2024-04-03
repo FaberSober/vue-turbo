b79bcc4478308501c3fde01d71435a7ae57b623c
xupengfei <faberxu@gmail.com>
2024-04-03 16:16:13 +0800

    add NProgress

--- apps/admin/src/typings/components.d.ts
+++ apps/admin/src/typings/components.d.ts
@@ -7,6 +7,10 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    NDialogProvider: (typeof import('naive-ui'))['NDialogProvider'];
+    NLoadingBarProvider: (typeof import('naive-ui'))['NLoadingBarProvider'];
+    NMessageProvider: (typeof import('naive-ui'))['NMessageProvider'];
+    NNotificationProvider: (typeof import('naive-ui'))['NNotificationProvider'];
     RouterLink: (typeof import('vue-router'))['RouterLink'];
     RouterView: (typeof import('vue-router'))['RouterView'];
   }
